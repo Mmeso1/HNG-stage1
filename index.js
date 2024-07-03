6964cea5fb2e99607026db6fb42a528fcebc5bd0
Mmeso1 <anisiubammesoma8@gmail.com>
2024-07-03 13:19:10 +0100

    done styling mobile view

--- index.js
+++ index.js
@@ -4,4 +4,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
   currentTimeUTC.innerHTML = `${new Date().toUTCString()} `;
   currentDay.innerHTML = `${new Date().toDateString().split(" ")[0]}`;
+
+  const headers = document.querySelectorAll(
+    ".accordion .accordion-item-header"
+  );
+
+  headers.forEach((header) => {
+    header.addEventListener("click", function () {
+      // Close currently open item
+      const openItem = document.querySelector(
+        '.accordion .accordion-item-body:not(:first-child):not([style*="display: none"])'
+      );
+      if (openItem) {
+        openItem.style.display = "none";
+        openItem.previousElementSibling.querySelector("span").innerHTML =
+          "&#43;"; // Change symbol to plus
+      }
+
+      // Toggle the clicked item
+      const body = this.nextElementSibling;
+      if (body.style.display === "block") {
+        body.style.display = "none";
+        this.querySelector("span").innerHTML = "&#43;"; // Change symbol to plus
+      } else {
+        body.style.display = "block";
+        this.querySelector("span").innerHTML = "&#8722;"; // Change symbol to minus
+      }
+    });
+  });
 });
